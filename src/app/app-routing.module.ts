14f9540953785a5a6359d68a6eb694e2fdb52911
PabloDefelitto <pdefelitto@gmail.com>
2024-07-09 12:07:05 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './heroes/pages/layout-page/layout-page.component';
+import { NewPageComponent } from './heroes/pages/new-page/new-page.component';
+import { SearchPageComponent } from './heroes/pages/search-page/search-page.component';
+import { ListPageComponent } from './heroes/pages/list-page/list-page.component';
+import { HeroPageComponent } from './heroes/pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {path: 'new-hero', component: NewPageComponent},
+      {path: 'search', component: SearchPageComponent},
+      {path: 'edit/:id', component: NewPageComponent},
+      {path: 'list', component: ListPageComponent},
+      {path: ':id', component: HeroPageComponent},
+      {path: '**', redirectTo: 'list'},
+      
+    ]
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
